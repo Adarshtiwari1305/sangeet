0f626c0189e58e64f383461fa89019f48bda0337
Adarshtiwari1305 <96828009+Adarshtiwari1305@users.noreply.github.com>
2022-10-07 10:18:06 +0530

    Update call.py

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -1,10 +1,10 @@
 #
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Copyright (C) 2021-2022 by Aadriti_fed@Github, < https://github.com/Jankarikiduniya >.
+# A Powerful Music Bot Property Of Hitler Indian Largest Chatting Group
 
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
+# Kanged By © @Shaitaan_bacchaa
+# Hitler © @aadriti_fed
+# Owner Adarsh tiwari
 # Harshit Sharma
 # All rights reserved. © Alisha © Alexa © Yukki
 
